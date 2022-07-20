ec5ed3255ba6154a293a30302dbbb250ccbe4900
Franklyn-Ukwueze <franklynifechukwu2@gmail.com>
2022-07-20 09:51:58 +0100

    created all functions for app

--- Student_database/studentapp.py
+++ Student_database/studentapp.py
@@ -5,7 +5,7 @@ client = pymongo.MongoClient("mongodb://localhost:27017")
 db = client["Student_db"]
 my_collection = db["student_data"]
 
-def add_new_student(name = ""):
+def add_new_student(name=""):
     split_input = []
     if name:
         split_input.append(name)
@@ -20,10 +20,10 @@ def add_new_student(name = ""):
         for i in range(len(split_input)):
             split_input[i] = split_input[i].strip()
 
-        subjects = list(input("Enter students subjects here, seperated by ',' then press ENTER\n"))
+        subjects = input("Enter students subjects here, seperated by ',' then press ENTER\n").split(", ")
 
         subject_scores = {}
-        for subject in subjects:
+        for _ in range(len(subjects)):
             data = input("Enter subject name and score seperated by ':'\n")
 
             temp = data.split(":")
@@ -76,7 +76,7 @@ def student_exists(student_data):
     else:
         return None
 
-def find_student(student_name):
+def find_student():
     """
     Searches database for student, if found it prints the student's 
     details else it prints 'Record not found.'
@@ -90,15 +90,45 @@ def find_student(student_name):
     else:
         print("Record not found.")
 
-def update_score(student_name):
+def update_score():
     """
-    Updates score of student provided and prints new details to screen.
+    Updates score of student provided and new details to screen.
     """
     student_name = input("Enter name of student who's score you would like to update.\n")
+    data = my_collection.find_one({"name": student_name}, { "_id": 0,"age": 0, "gender": 0,
+                                "students_class": 0,"subjects":0})
+    
+    print(f"{student_name}'s scores are:\n{data}")
+    number_of_subjects =int(input("Enter number of subjects student has registered. "))
+
+    new_scores = {}
+    for _ in range(number_of_subjects):
+        data = input("Enter name of subject and new score seperated by ':'\n")
+
+        temp = data.split(":")
+        new_scores[temp[0]] = int(temp[1])
+
+    score_list = []
+    for score in new_scores.values():
+        score_list.append(score)
+    new_total = sum(score_list)
+
+    update_filter = {"name":student_name}
+    update_value = {"$set": {"scores": new_scores}}
+    my_collection.update_one(update_filter, update_value)
+
+    update_value = {"$set": {"total_score": new_total}}
+    my_collection.update_one(update_filter, update_value)
+
+    data = my_collection.find_one({"name": student_name})
+    print(f"{student_name}'s new scores are:\n{data}")
     
 
 
 
+#add_new_student()
+#find_student()
+#update_score()
 
 
 
